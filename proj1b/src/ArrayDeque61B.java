b710a56d1c64a280589732d0c9660152f132028d
fairy_08 <2659657324@qq.com>
2025-08-28 23:13:05 +0800

    实现了addFirst、addLast、toList、isEmpty、size、get

--- proj1b/src/ArrayDeque61B.java
+++ proj1b/src/ArrayDeque61B.java
@@ -1,3 +1,82 @@
-public class ArrayDeque61B {
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.Math;
+
+public class ArrayDeque61B<T> implements Deque61B<T> {
+
+    private T[] items;
+    private int size;
+    private int front;
+    private int rear;
+
+    public ArrayDeque61B() {
+        items = (T[]) new Object[8];
+        size = 0;
+        front = 0;
+        rear = 0;
+    }
+
+    @Override
+    public void addFirst(T x) {
+        front = Math.floorMod(front - 1, items.length);
+        items[front] = x;
+        size++;
+    }
+
+    @Override
+    public void addLast(T x) {
+        items[rear] = x;
+        rear = rear + 1;  // rear = Math.floorMod(rear + 1; items.length);
+        size++;
+    }
+
+    @Override
+    public List<T> toList() {
+        List<T> returnList = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            returnList.addLast(get(i));
+        }
+        return returnList;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public T removeFirst() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeFirst'");
+    }
+
+    @Override
+    public T removeLast() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeLast'");
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        int i = Math.floorMod(front + index, items.length);
+        return items[i];
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRecursive'");
+    }
 
 }
--- proj1b/tests/ArrayDeque61BTest.java
+++ proj1b/tests/ArrayDeque61BTest.java
@@ -10,14 +10,80 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDeque61BTest {
 
-//     @Test
-//     @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
-//     void noNonTrivialFields() {
-//         List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
-//                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
-//                 .toList();
-//
-//         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
-//     }
+    @Test
+    @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
+    void noNonTrivialFields() {
+        List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
+                .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
+                .toList();
+
+        assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
+    }
+
+    @Test
+    public void addFirstTest() {
+        Deque61B<Integer> lst = new ArrayDeque61B<>();
+
+        for (int i = 7; i >= 0; i--) {
+            lst.addFirst(i);
+        }
+        assertThat(lst.toList()).containsExactly(0, 1, 2, 3, 4, 5, 6, 7).inOrder();
+
+        // 扩容
+        // lst.addFirst(8);
+        // assertThat(lst.toList()).containsExactly(8, 0, 1, 2, 3, 4, 5, 6, 7).inOrder();
+    }
+
+    @Test
+    public void addLastTest() {
+        Deque61B<Integer> lst = new ArrayDeque61B<>();
+
+        for (int i = 0; i < 8; i++) {
+            lst.addLast(i);
+        }
+        assertThat(lst.toList()).containsExactly(0, 1, 2, 3, 4, 5, 6, 7).inOrder();
+
+        // 扩容
+        // lst.addLast(8);
+        // assertThat(lst.toList()).containsExactly(0, 1, 2, 3, 4, 5, 6, 7, 8).inOrder();
+    }
+
+    @Test
+    public void isEmptyTest() {
+        Deque61B<Integer> lst = new ArrayDeque61B<>();
+
+        assertThat(lst.isEmpty()).isTrue();
+        lst.addFirst(1);
+        assertThat(lst.isEmpty()).isFalse();
+    }
+
+    @Test
+    public void sizeTest() {
+        Deque61B<Integer> lst = new ArrayDeque61B<>();
+
+        assertThat(lst.size()).isEqualTo(0);
+        lst.addLast(0);
+        assertThat(lst.size()).isEqualTo(1);
+        lst.addFirst(1);
+        assertThat(lst.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void getTest() {
+        Deque61B<Integer> lst = new ArrayDeque61B<>();
+
+        lst.addLast(0);   // [0]
+        lst.addLast(1);   // [0, 1]
+        lst.addFirst(-1); // [-1, 0, 1]
+        lst.addLast(2);   // [-1, 0, 1, 2]
+        lst.addFirst(-2); // [-2, -1, 0, 1, 2]
+
+        assertThat(lst.get(2)).isEqualTo(0);
+        assertThat(lst.get(4)).isEqualTo(2);
+        assertThat(lst.get(-1)).isEqualTo(null);
+        assertThat(lst.get(50)).isEqualTo(null);
+
+        // 扩容
+    }
 
 }
